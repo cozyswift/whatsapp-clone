8ecc8d61eba1040ffdcc2b189a010b99f9bd00b6
cozyswift <cozyswift@gmail.com>
2019-11-17 23:19:05 +0900

    3day

--- client/src/components/ChatsListScreen/ChatsList.tsx
+++ client/src/components/ChatsListScreen/ChatsList.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { chats } from '../../db';
 import moment from 'moment';
 import { List, ListItem } from '@material-ui/core';
 import styled from 'styled-components';
+import { useState, useMemo } from 'react';
 
 const Container = styled.div`
   height: calc(100% - 56px);
@@ -50,21 +50,29 @@ const MessageDate = styled.div`
 `;
 
 function ChatsList() {
+  const [chats, setChats] = useState<any[]>([]);
+
+  useMemo(async () => {
+    const body = await fetch(`${process.env.REACT_APP_SERVER_URL}`);
+    const chats = await body.json();
+    setChats(chats);
+  }, []);
+
   return (
     <Container>
       <StyledList>
         {chats.map(chat => (
-          <StyledListItem key={chat.id} button>
+          <StyledListItem key={chat!.id} button>
             {/* key영역에 id값을 부여 함으로써 각 엘리먼트를 구별할 수 있도록 해준다. */}
             <ChatPicture src={chat.picture} alt="Profile" />
             <ChatInfo>
               <ChatName>{chat.name}</ChatName>
-              {/* return 값이 없을 수 있는 상황에서는 반드시 null인지 undifineded인지 유효성 검사를 해주어야 한다 */}
               {chat.lastMessage && (
+                // {/* return 값이 없을 수 있는 상황에서는 반드시 null인지 undifineded인지 유효성 검사를 해주어야 한다 */}
                 <React.Fragment>
                   <MessageContent>{chat.lastMessage.content}</MessageContent>
                   <MessageDate>
-                    {moment(chat.lastMessage.createdAt).format('HH:mm')}
+                    {moment(chat.lastMessage.createdAt).format(`HH:mm`)}
                   </MessageDate>
                 </React.Fragment>
               )}
@@ -75,5 +83,32 @@ function ChatsList() {
     </Container>
   );
 }
+
+// function ChatsList() {
+//   return (
+//     <Container>
+//       <StyledList>
+//         {chats.map(chat => (
+//           <StyledListItem key={chat.id} button>
+//             {/* key영역에 id값을 부여 함으로써 각 엘리먼트를 구별할 수 있도록 해준다. */}
+//             <ChatPicture src={chat.picture} alt="Profile" />
+//             <ChatInfo>
+//               <ChatName>{chat.name}</ChatName>
+//               {/* return 값이 없을 수 있는 상황에서는 반드시 null인지 undifineded인지 유효성 검사를 해주어야 한다 */}
+//               {chat.lastMessage && (
+//                 <React.Fragment>
+//                   <MessageContent>{chat.lastMessage.content}</MessageContent>
+//                   <MessageDate>
+//                     {moment(chat.lastMessage.createdAt).format('HH:mm')}
+//                   </MessageDate>
+//                 </React.Fragment>
+//               )}
+//             </ChatInfo>
+//           </StyledListItem>
+//         ))}
+//       </StyledList>
+//     </Container>
+//   );
+// }
 
 export default ChatsList;
